7b54b923da11104b155c62fd9fb88af2bd4a13b6
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-21 16:29:30 +0200

    start of creation

---
+++ Project/tic-tac-toe.py
@@ -0,0 +1,26 @@
+import colorama
+import numpy as np
+from colorama import Fore, Style, init
+
+init()
+print(Style.BRIGHT)
+
+
+class Player():
+    def __init__(self, name):
+        self.name = neme
+
+class Field():
+    def new_field(self):
+        pass
+    
+def startgame():
+    print("\tHELLO TIC-TAC-TOE")
+
+    
+    
+    
+startgame()    
+    
+
+    
