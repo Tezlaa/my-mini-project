d9170ee3d23da19ef72adc3257bbcebce1f43593
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-05 01:40:35 +0200

    new project"

---
+++ Project/Calculator Tkinter/calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+main_menu = Tk()
+
+main_menu.geometry("300x400+1500+100")
+main_menu.resizable(False, False)
+main_menu.title("CALCULATOR")
+
+
+main_menu.mainloop()
