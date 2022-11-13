2ecfdb0ab035f26c470442f46d4c270b58a11bdc
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-13 04:21:04 +0200

    finish

--- Miscellaneous/Tree-sort.py
+++ Miscellaneous/Tree-sort.py
@@ -1,4 +1,6 @@
-from random import
+from random import *
+from time import *
+import csv
 
 class Node():
     def __init__(self, digit):
@@ -24,10 +26,57 @@ def insert_in_tree(root, unit):
                 
 def sort_max(root):
     if root != None:
-        sort_max(root.right)
-        print(root.digit)
         sort_max(root.left)
+        # print(root.digit)
+        sort_max(root.right)
 
-tree = Node(r)
 
-sort_max(tree)
+if __name__=="__main__":
+    
+    finall_array = []
+    result = []
+    cycle_quality = 10000
+    
+    tree = Node(randint(1, cycle_quality))
+    
+    """TEST FIRST. WITH USING TREE SORT"""
+    start = time()
+    
+    for cycle in range(cycle_quality):
+        insert_in_tree(tree, Node(randint(1, cycle_quality)))
+    sort_max(tree)
+    
+    stop = time()
+    difference = stop-start
+    result.append(["Tree sort", difference])
+    
+    """TEST SECOND. WITH USING STANDART SORT"""
+    start = time()
+    
+    for cycle in range(cycle_quality):
+        finall_array.append(randint(1, cycle_quality))
+    finall_array.sort()
+    # for cycle in range(cycle_quality):
+    #     print(finall_array[cycle])
+    
+    stop = time()
+    difference = stop-start
+    result.append(["Standart sort", difference])
+    
+    """RESULT"""
+    with open("data.csv", "r", encoding='UTF-8') as file:
+        reader = csv.reader(file)
+        
+        result.append([])#add indent
+        
+        for object_in_file in reader:
+            result.append(object_in_file)
+            
+
+    with open('data.csv', "w", newline='', encoding='UTF-8') as file:
+        writer = csv.writer(file)
+        for i in result:
+            writer.writerow(i)
+        
+        
+    #print(f'\n\nTree sort 1 - \t{result}\nsort() 2 - \t{result2}\n\n')
