47b04a7b0c1a0d19f9a0574caf8a08d2086191bb
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-12-14 23:59:06 +0200

    fix bug and add method

--- Project/console-dictionary/dictionary.py
+++ Project/console-dictionary/dictionary.py
@@ -10,40 +10,46 @@ class Dictionary:
         except:
             with open("data.csv", "w", newline=""):
                 pass
-            
+        
         self.header = ["English", "Transcript", "Translation"]
-            
+        
         if (os.stat("data.csv").st_size == 0) == True:
             with open("data.csv", "w", newline="") as f:
                 writer = csv.DictWriter(f, fieldnames=self.header)
                 writer.writeheader()   
-    
-    def set_word(self, word_on_eng, transcript, translation):
-        with open("data.csv", "r", newline="",  encoding="utf8") as f:
-            reader = csv.reader(f)
-
-            with open("data.csv", "w", newline="",  encoding="utf8") as f:
-                writer = csv.DictWriter(f, self.header)
-                writer.writeheader()
-        
-                new_row = {"English":word_on_eng,
-                        "Transcript": transcript,
-                        "Translation":translation,
-                        }
                 
-                for line in reader:
-                    writer.writerow(line)
-                writer.writerow(new_row)
-                    
-              
+                f.close()
+    
+    def set_word(self, word_on_eng: str, transcript: str, translation: str):
+        """Word on english, transcript, translation"""
+        
+        with open("data.csv", "a", newline="",  encoding="utf8") as f:
+            new_row = [word_on_eng, transcript, translation]
+            
+            writer = csv.writer(f)
+            writer.writerow(new_row)
+            
+            f.close()
+
     def get_all_word(sefl):
-        with open("data.csv", "r", newline="") as f:
+        with open("data.csv", "r", newline="", encoding="utf8") as f:
             for line in csv.reader(f):
                 print(line)
+            
+            f.close()
+    
+    def clear(self):
+        with open("data.csv", "w", newline="", encoding="utf8") as f:
+            writer = csv.DictWriter(f, fieldnames=self.header)
+            writer.writeheader()
+            
+            f.close() 
 
 if __name__=="__main__":
     dictionary = Dictionary()
     
-    dictionary.set_word("Opklhsdjfdgd", "опеsdн", "открыdsfть")
-
+    #dictionary.set_word("Open", "опэн", "Открыть")
+    
+    dictionary.get_all_word()
+    
     
