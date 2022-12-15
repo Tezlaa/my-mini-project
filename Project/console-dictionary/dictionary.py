2ff9633a26b940ca9fea2c7094a4adf6ef49a52f
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-12-15 17:45:09 +0200

    created method with remove row from csv file

--- Project/console-dictionary/dictionary.py
+++ Project/console-dictionary/dictionary.py
@@ -1,55 +1,81 @@
 import csv
 import os
 
+
 class Dictionary:
-    def __init__(self):
+    def __init__(self, name_file: str):
         
-        #file check
+        self.name_file = f'{name_file}.csv'
+
+        # file check
         try:
-            os.stat("data.csv").st_size == 0
+            os.stat(self.name_file).st_size == 0
         except:
-            with open("data.csv", "w", newline=""):
+            with open(self.name_file, "w", newline=""):
                 pass
-        
+
         self.header = ["English", "Transcript", "Translation"]
-        
-        if (os.stat("data.csv").st_size == 0) == True:
-            with open("data.csv", "w", newline="") as f:
+
+        if os.stat(self.name_file).st_size == 0:
+            with open(self.name_file, "w", newline="") as f:
                 writer = csv.DictWriter(f, fieldnames=self.header)
-                writer.writeheader()   
-                
+                writer.writeheader()
+
                 f.close()
-    
+
     def set_word(self, word_on_eng: str, transcript: str, translation: str):
         """Word on english, transcript, translation"""
-        
-        with open("data.csv", "a", newline="",  encoding="utf8") as f:
+
+        with open(self.name_file, "a", newline="", encoding="utf8") as f:
             new_row = [word_on_eng, transcript, translation]
-            
+
             writer = csv.writer(f)
             writer.writerow(new_row)
-            
+
             f.close()
 
-    def get_all_word(sefl):
-        with open("data.csv", "r", newline="", encoding="utf8") as f:
+    def get_all_word(self):
+        with open(self.name_file, "r", newline="", encoding="utf8") as f:
             for line in csv.reader(f):
                 print(line)
-            
+
             f.close()
-    
+
+    def remove_row(self, column_number: int):
+        
+        counter = 0
+        with open(self.name_file, "r", encoding="utf8") as f_data:
+            reader = csv.reader(f_data)
+
+            with open("tamp.csv", "w", newline="", encoding="utf8") as f_tamp:
+                writer = csv.writer(f_tamp)
+                for r in reader:
+                    if counter == column_number:
+                        counter += 1
+                        continue
+                    writer.writerow(r)                    
+                    counter += 1
+            f_data.close()
+            
+            os.remove(self.name_file)
+        os.rename("tamp.csv", self.name_file)
+                    
     def clear(self):
-        with open("data.csv", "w", newline="", encoding="utf8") as f:
+        with open(self.name_file, "w", newline="", encoding="utf8") as f:
             writer = csv.DictWriter(f, fieldnames=self.header)
             writer.writeheader()
-            
-            f.close() 
 
-if __name__=="__main__":
-    dictionary = Dictionary()
+            f.close()
+
+
+if __name__ == "__main__":
+    dictionary = Dictionary("data")
     
-    #dictionary.set_word("Open", "опэн", "Открыть")
+    dictionary.set_word("Open", "опэн", "Открыть") 
     
     dictionary.get_all_word()
+
+    dictionary.clear()
+    
     
     
