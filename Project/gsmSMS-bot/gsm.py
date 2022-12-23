9e7f5f248c20521de7b5f3fad6b05d54c3ac6225
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-12-23 23:57:43 +0200

    add connect in gsm

--- Project/gsmSMS-bot/gsm.py
+++ Project/gsmSMS-bot/gsm.py
@@ -1,6 +1,13 @@
+from __future__ import print_function
+from datetime import datetime
+from gsmmodem.modem import GsmModem, Sms
+
+import logging
+
 import serial.tools.list_ports
 import time
 import sys
+
 
 list = serial.tools.list_ports.comports()
 connected = []
@@ -31,24 +38,37 @@ for port in ports:
         pass
 print("Availible COM Ports: " + str(result))
 
-ser = serial.Serial(
-    port=result[0],
-    baudrate=s.baudrate,
-    bytesize=s.bytesize,
-    parity=s.parity,
-    stopbits=s.stopbits,
-    timeout=0.05)
+PORT = str(result[0])
+BAUDRATE = s.baudrate
+PIN = None
 
-print(ser.readline())
+def handle_sms(sms):
+    print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
 
-time.sleep(4)
+def main():
+    print('Initializing modem...')
 
-# ser.write("AT+CMGL=\"ALL\"\r\n".encode("utf-8"))
+    # Uncomment the following line to see what the modem is doing:
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=handle_sms)
+    modem.smsTextMode = False
+    modem.connect(PIN)
 
-# while True:
-#     gelen = str(ser.readline().decode("UTF-8", errors='replace'))
+    #modem.processStoredSms(False)
+    smss = modem.listStoredSms(status=Sms.STATUS_ALL, delete=True);
+    print(str(len(smss)) + ' messages found')
 
-#     gelen = gelen.replace('\r', '').replace('\n', '')
-#     if len(gelen) > 0:
-#         print(gelen)
+    for i in range(len(smss)):
+        sms = smss[i]
+        handleSms(sms)
 
+    print('Waiting for new SMS message...')    
+    try:    
+        modem.rxThread.join(2**31)
+    finally:
+        modem.close();
+
+if __name__ == '__main__':
+    main()
+
+    input()
