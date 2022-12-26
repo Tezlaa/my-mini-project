d84b2692c324bdd16ec4c4a4df0e863142a771b7
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-12-26 20:31:01 +0200

    edit

--- Project/gsmSMS-bot/gsm.py
+++ Project/gsmSMS-bot/gsm.py
@@ -1,56 +1,57 @@
-from __future__ import print_function
-from datetime import datetime
-from gsmmodem.modem import GsmModem, Sms
-
-import logging
-
-import serial.tools.list_ports
 import time
 import sys
 
+from datetime import datetime
+from gsmmodem.modem import GsmModem, Sms
 
-class Gms_conect:
+import serial.tools.list_ports
+import logging
+
+
+class Gsm:
     def __init__(sefl):
         pass
 
-    def connect_gms(self, connect: list, pin: int=None) -> None:
-    
+    def connect_gsm(self, connect: list, pin: int = None) -> None:
+
         try:
             s = serial.Serial(connect)
             s.close()
         except (OSError, serial.SerialException):
             return "Error"
-        
         print(s)
-        
-        
+
         PORT = s.port
         BAUDRATE = s.baudrate
         PIN = pin
-        
+
         #    Uncomment the following line to see what the modem is doing:
-        logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-        modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=self.handle_sms)
+        logging.basicConfig(
+            format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
+        modem = GsmModem(
+            PORT, BAUDRATE, smsReceivedCallbackFunc=self.handle_sms)
+
         modem.smsTextMode = False
         modem.connect(PIN)
 
         time.sleep(4)
-            
+
     def check(self) -> None:
-        
-        smss = modem.listStoredSms(status=Sms.STATUS_ALL, delete=True);
+
+        smss = modem.listStoredSms(status=Sms.STATUS_ALL, delete=True)
         print(str(len(smss)) + ' messages found')
 
         for i in range(len(smss)):
             sms = smss[i]
             handleSms(sms)
 
-        print('Waiting for new SMS message...')    
-        try:    
+        print('Waiting for new SMS message...')
+        try:
             modem.rxThread.join(2**31)
         finally:
-            modem.close();
-    
+            modem.close()
+
     def close(sefl) -> None:
         modem.close()
 
@@ -68,8 +69,6 @@ class Gms_conect:
     def handle_sms(sms) -> str:
         return f'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text)
 
-
-
     # # if sys.platform.startswith('win'):
 
     # #     ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -81,8 +80,6 @@ class Gms_conect:
     # #     ports = glob.glob('/dev/tty.*')
     # # else:
     # #     raise EnvironmentError('Unsupported platform')
-
-
 
     # index_with_connect = 0
     # try:
--- Project/gsmSMS-bot/main.py
+++ Project/gsmSMS-bot/main.py
@@ -1,23 +1,21 @@
-from gsm import Gms_conect
+from gsm import Gsm
 
 
-if __name__=="__main__":
-    main = Gms_conect()
+if __name__ == "__main__":
+    main = Gsm()
 
     all_connect = main.get_all_connect()
     
     print(all_connect, end="\n\n")
     
     for module in all_connect:
-        
-        connect = main.connect_gms(module)
-        
+        connect = main.connect_gsm(module)
         if connect != "Error":
             main.check()
-
             main.close()
         else:
             print(f'{module} - error')
     
     input("end")
+    
     
